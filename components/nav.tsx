be71b0c0dc170fcbae058efabd680e4872c3357d
lawynnjana <jlawynn@gmail.com>
2021-03-16 14:17:43 -0400

    remove logo in navbar

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import Link from "next/link";
 
 import { NavSection, StyledHamburger } from "./styles/nav.styles";
-import Logo from "./logo";
 import { Container } from "./container";
 import SiteConfig from "../config/index.json";
 import { MenuContext } from ".";
@@ -19,7 +18,6 @@ const Nav = () => {
           <div className="navLeft">
             <Link href="/">
               <a className="no-underline">
-                <Logo />
                 <span>{SiteConfig.author.name}</span>
               </a>
             </Link>
